ce5b169d406c7678b63d30a2e52cfc02823f6d37
Lisa Yang <lisayang041@gmail.com>
2022-06-11 21:23:17 +0800

    Added list and bookmark controllers

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,38 @@
 class BookmarksController < ApplicationController
+  before_action :find_bookmark, only: :destroy
+  before_action :find_list, only: :create
+
   def new
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.save
-    redirect_to lists_path(@list)
+    @bookmark.list = @list
+      if @bookmark.save
+        redirect_to list_path(@list)
+      else
+        render :new
+      end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to lists_path(@bookmark.list)
   end
 
   private
+
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie, :list)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
+
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'lists#index'
 
   resources :lists do
-    resources :bookmarks, only: [ :new, :create, :delete ]
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
 end
