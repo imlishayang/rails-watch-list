1e09f0c9aed3e3dd587ac4913633b027738339b4
Lisa Yang <lisayang041@gmail.com>
2022-06-16 17:00:28 +0800

    Install typed.js

---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,12 @@
+// import { Controller } from '@hotwired/stimulus';
+// import Typed from 'typed.js';
+
+// export default class extends Controller {
+//   connect() {
+//     new Typed(this.element, {
+//       strings: ["First sentence.","\n Second sentence"],
+//       // strings: ['Hello''^1000\n'' `installing components...` ^1000\n `Fetching from source...`'],
+//       typeSpeed: 40,
+//     });
+//   }
+// }
